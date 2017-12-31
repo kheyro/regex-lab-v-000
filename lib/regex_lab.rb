99c3811c4aa638ef175be16bdc9656b8eb3d36c8
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-31 10:15:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  !!word.capitalize.match(/\A+[AEIOU]/)
+  !!word.capitalize.match(/\A[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\Aun.*ing\z\s/)
 end
 
 def words_five_letters_long(text)
